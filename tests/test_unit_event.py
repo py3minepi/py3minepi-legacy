bbd4241004aa79c34ef9776025f19c67a3caf0e5
HashBang <hashbangstudio@users.noreply.github.com>
2014-10-01 21:36:53 +0100

    Revert event repr to previous and test
    revert to previous style of repr
    Add test for original event

--- tests/test_unit_event.py
+++ tests/test_unit_event.py
@@ -21,7 +21,7 @@ class TestEvent(unittest.TestCase):
         data = [0, 14, 15, 16, 1, 1]
         event = BlockEvent(data[0], data[1], data[2],
                           data[3], data[4], data[5])
-        expected = "BlockEvent({}, {:2f}, {:2f}, {:2f}, {:d}, {:d})".format(
+        expected = "BlockEvent({}, {:2f}, {:2f}, {:2f}, {:2f}, {})".format(
             data[0], data[1], data[2], data[3], data[4], data[5])
         rep = repr(event)
         self.assertTrue(rep, expected)
