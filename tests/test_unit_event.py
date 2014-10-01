4da234cdf11efc8a1d9bae5149b4aeeef6bcc3a2
HashBang <hashbangstudio@users.noreply.github.com>
2014-10-01 21:05:46 +0100

    Change evnt to event and remove setUp/teardown

--- tests/test_unit_event.py
+++ tests/test_unit_event.py
@@ -4,45 +4,39 @@ from minecraft.event import BlockEvent
 
 class TestEvent(unittest.TestCase):
 
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
     def testInstantiation(self):
-        evntType = 0
+        eventType = 0
         pos = [14, 15, 16]
         face = 2
         entity = 1
-        evnt = BlockEvent(evntType, pos[0], pos[1], pos[2], face, entity)
-        self.assertEqual(evnt.type, evntType)
-        self.assertEqual(evnt.pos.x, pos[0])
-        self.assertEqual(evnt.pos.y, pos[1])
-        self.assertEqual(evnt.pos.z, pos[2])
-        self.assertEqual(evnt.face, face)
-        self.assertEqual(evnt.entityId, entity)
+        event = BlockEvent(eventType, pos[0], pos[1], pos[2], face, entity)
+        self.assertEqual(event.type, eventType)
+        self.assertEqual(event.pos.x, pos[0])
+        self.assertEqual(event.pos.y, pos[1])
+        self.assertEqual(event.pos.z, pos[2])
+        self.assertEqual(event.face, face)
+        self.assertEqual(event.entityId, entity)
 
     def testRepresentation(self):
         data = [0, 14, 15, 16, 1, 1]
-        evnt = BlockEvent(data[0], data[1], data[2],
+        event = BlockEvent(data[0], data[1], data[2],
                           data[3], data[4], data[5])
         expected = "BlockEvent({}, {:2f}, {:2f}, {:2f}, {:d}, {:d})".format(
             data[0], data[1], data[2], data[3], data[4], data[5])
-        rep = repr(evnt)
+        rep = repr(event)
         self.assertTrue(rep, expected)
 
     def testStaticHit(self):
         x = 89
         y = -34
         z = 30
-        evntType = 0
+        eventType = 0
         face = 3
         entity = 1
-        evnt = BlockEvent(BlockEvent.HIT, x, y, z, face, entity)
-        self.assertEqual(evnt.type, evntType)
-        self.assertEqual(evnt.pos.x, x)
-        self.assertEqual(evnt.pos.y, y)
-        self.assertEqual(evnt.pos.z, z)
-        self.assertEqual(evnt.face, face)
-        self.assertEqual(evnt.entityId, entity)
+        event = BlockEvent(BlockEvent.HIT, x, y, z, face, entity)
+        self.assertEqual(event.type, eventType)
+        self.assertEqual(event.pos.x, x)
+        self.assertEqual(event.pos.y, y)
+        self.assertEqual(event.pos.z, z)
+        self.assertEqual(event.face, face)
+        self.assertEqual(event.entityId, entity)
