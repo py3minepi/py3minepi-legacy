py3minepi/py3minepi-legacy

commit e9f5ce62431757bd0a98fae39913e3138c2acb5b

Only need to run envvars.sh when inside chroot

.travis-ci.sh
@@ -51,8 +51,6 @@ function setup_arm_chroot {
 }
 
 function run_tests {
-	. ./envvars.sh
-
 	echo "--- Running tests"
 	echo "--- Environment: $(uname -a)"
 	echo "--- Working directory: $(pwd)"
@@ -65,6 +63,8 @@ function run_tests {
 if [ "${ARCH}" = "arm" ]; then
   if [ -e "/.chroot_is_done" ]; then
     echo "--- Running inside chrooted environment"
+
+    . ./envvars.sh
 
     run_tests
   else

commit 95efb92790848f96832eb64e6b76d3f7faa72f04

Source envvars.sh inside run_tests if it exists
Slightly clutching at straws here but it seems like
e9f5ce62431757bd0a98fae39913e3138c2acb5b broke the tox filter; I believe
that's because of environment variables / lack of export / subshell
mechanics that this _should_ fix...

.travis-ci.sh
@@ -51,6 +51,10 @@ function setup_arm_chroot {
 }
 
 function run_tests {
+	if [ -f "./envvars.sh" ]; then
+		. ./envvars.sh
+	fi
+
 	echo "--- Running tests"
 	echo "--- Environment: $(uname -a)"
 	echo "--- Working directory: $(pwd)"
@@ -63,8 +67,6 @@ function run_tests {
 if [ "${ARCH}" = "arm" ]; then
   if [ -e "/.chroot_is_done" ]; then
     echo "--- Running inside chrooted environment"
-
-    . ./envvars.sh
 
     run_tests
   else