38f99be915a0cc59e93de763ac7784da5efc7c83
Kristian Glass <git@doismellburning.co.uk>
2014-10-05 19:20:33 +0100

    Fixed flake8 objections

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -1,11 +1,13 @@
 """
 API compatibility tests based on http://www.raspberrypi.org/documentation/usage/minecraft/
 
-We do not want to break this API - we do not want to be responsible for sad children (and adults) whose awesome Minecraft code no longer works.
+We do not want to break this API - we do not want to be responsible for sad children
+(and adults) whose awesome Minecraft code no longer works.
 
 Ergo this suite is a translation of that usage guide
 
-Currently it doesn't actually test the __success__ of any of these commands, but it at least verifies that the commands still exist, which is the most likely cause of breakage
+Currently it doesn't actually test the __success__ of any of these commands, but it at
+least verifies that the commands still exist, which is the most likely cause of breakage
 """
 
 import pytest
@@ -20,8 +22,10 @@ from time import sleep
 def mc(monkeypatch):
     monkeypatch.setattr("socket.socket.connect", lambda x, y: None)
     monkeypatch.setattr("socket.socket.sendall", lambda x, y: None)
+
     def dummy_send(_, *args):
         return None
+
     monkeypatch.setattr("mcpi.connection.Connection.send", dummy_send)
     monkeypatch.setattr("mcpi.minecraft.CmdPositioner.getPos", lambda x, y: Vec3(0.1, 0.1, 0.1))
     return minecraft.Minecraft.create()
@@ -37,12 +41,12 @@ def test_get_pos(mc):
 
 def test_teleport(mc):
     x, y, z = mc.player.getPos()
-    mc.player.setPos(x, y+100, z)
+    mc.player.setPos(x, y + 100, z)
 
 
 def test_set_block(mc):
     x, y, z = mc.player.getPos()
-    mc.setBlock(x+1, y, z, 1)
+    mc.setBlock(x + 1, y, z, 1)
 
 
 def test_blocks_as_variables(mc):
@@ -62,7 +66,7 @@ def test_special_blocks(mc):
 def test_set_blocks(mc):
     stone = 1
     x, y, z = mc.player.getPos()
-    mc.setBlocks(x+1, y+1, z+1, x+11, y+11, z+11, stone)
+    mc.setBlocks(x + 1, y + 1, z + 1, x + 11, y + 11, z + 11, stone)
 
 
 def test_dropping_blocks_as_you_walk(mc):
